cf5b85f47bce9ddb660fefa0ee7eabe1731ee560
nisiko36 <89366027+nisiko36@users.noreply.github.com>
2022-11-04 17:54:35 +0900

    models create

--- music_app/models.py
+++ music_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+# [時間、曲名、アーティスト名、Switch]
+
+class SpotifyAlarmModel(models.Model):
+    time = models.TimeField()
+    track_name = models.CharField(max_length=100)
+    artist_name = models.CharField(max_length=100)
+    switch = models.BooleanField()
+
+    def __str__(self):
+        return self.track_name
+
+
+
+
