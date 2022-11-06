3d1f03edd3e423a0b641e7bd6dde0ba1d8797894
nisiko36 <89366027+nisiko36@users.noreply.github.com>
2022-11-06 20:22:40 +0900

    UpdateView

--- music_app/urls.py
+++ music_app/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import homefunc, listfunc
+from .views import homefunc, listfunc, detailfunc, Create, deletefunc, Update
 
 urlpatterns = [
     path('home/', homefunc, name='home'),
-    path('list/', listfunc, name='list')
+    path('list/', listfunc, name='list'),
+    path('detail/<int:pk>', detailfunc, name='detail'),
+    path('create/', Create.as_view(), name='create'),
+    path('delete/<int:pk>', deletefunc, name='delete'),
+    path('update/<int:pk>', Update.as_view(), name='update'),
 ]
--- music_app/views.py
+++ music_app/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic import CreateView, UpdateView
 # from api.get_token import get_token
 from api.noname import test
 from api.create_playlist import create_playlist
+from .models import SpotifyAlarmModel
+from django.urls import reverse_lazy
+
+
 
 
 # Create your views here.
@@ -12,5 +17,46 @@ def homefunc(request):
     return render(request, 'home.html',  context)
 
 def listfunc(request):
-    context = {'list1':[1,2,3]}
+    object = SpotifyAlarmModel.objects.all()
+    context = {'object': object,}
     return render(request, 'list.html', context)
+
+def detailfunc(request,pk):
+    object = SpotifyAlarmModel.objects.get(pk=pk)
+    context = {'object': object}
+    return render(request, 'detail.html', context)
+
+class Create(CreateView):
+    template_name = 'create.html'
+    model = SpotifyAlarmModel
+    fields = ('time','track_name','artist_name','switch')
+    success_url = reverse_lazy('list')
+
+def deletefunc(request,pk):
+    object = SpotifyAlarmModel.objects.filter(pk=pk)
+    context = {'object':object}
+    if request.method == 'POST':
+        object.delete()
+        return redirect('list')
+    else:
+        return render(request, 'delete.html', context)
+
+# def updatefunc(request, pk):
+#     object = SpotifyAlarmModel.objects.get(pk=pk)
+#     context = {'object':object}
+#     if request.method == 'POST':
+#         object.time = request.POST.get('time')
+#         object.track_name = request.POST.get('track_name')
+#         object.artist_name = request.POST.get('artist_name')
+#         object.switch = request.POST.get('switch')
+#         object.save()
+#     else:
+#         return render(request, 'update.html', context)
+
+class Update(UpdateView):
+    temlate_name = 'update.html'
+    model = SpotifyAlarmModel
+    fields = ['time','track_name','artist_name','switch']
+    success_url = reverse_lazy('list')
+
+
